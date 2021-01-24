5511b3006a9e4a6c25859b5a97b6fdc6822cd77d
Chad LeJeune <chad.lejeune23@gmail.com>
2021-01-24 14:06:42 -0600

    Created and started main JavaScript file

---
+++ random_message_generator.js
@@ -0,0 +1,24 @@
+const swimmerNames = ['Michael', 'Jenni', 'Chad', 'Victoria', 'Matt']
+
+const events = [
+    {distance: 100, stroke: 'Breast Stroke', record: '47'},
+    {distance: 200, stroke: 'Breast Stroke', record: '100'},
+    {distance: 100, stroke: 'Butterfly', record: '43'},
+    {distance: 200, stroke: 'Butterfly', record: '94'},
+    {distance: 100, stroke: 'Backstroke', record: '45'},
+    {distance: 200, stroke: 'Backstroke', record: '96'},
+    {distance: 100, stroke: 'Freestyle', record: '40'},
+    {distance: 200, stroke: 'Freestyle', record: '87'},
+]
+
+const generate_random_time = distance => {
+    if (distance === 100) {return Math.floor(Math.random() * 65) + 35}  // Outputs times between 35 and 100 seconds.  
+    if (distance === 200) {return Math.floor(Math.random() * 100) + 80}  // Outputs times between 80 and 180 seconds.  
+    else {console.log('Please input a valid distance (100 or 200).')}
+}
+
+const generate_random_message = () => {
+    let swimmer = swimmerNames[Math.floor(Math.random() * swimmerNames.length)]
+    let event = events[Math.floor(Math.random() * events.length)]
+    
+}
